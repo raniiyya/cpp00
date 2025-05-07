b2b84f8df42167c7002f0adff50bb74843fa984f
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2025-05-07 09:01:48 +0000

    updated

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int ac, char **av) {
+    if (ac == 1) {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return 0;
+    }
+    
+
+}
